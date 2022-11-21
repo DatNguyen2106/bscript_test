de5e5f063338f559bbb785cd7c38db2c9abea0db
DatNguyen2106 <nlddat2000@gmail.com>
2022-11-21 18:25:58 +0700

    update 21/11 afternoon
    add registration + assessment

--- server/routers/student.js
+++ server/routers/student.js
@@ -5,7 +5,12 @@ const db = require('./../db/connectDB')
 const student_get_router = require('./student/student_get');
 const student_registrationBachelorThesis_router = require('./student/student_registrationBachelorThesis')
 const student_registrationOralDefense_router = require('./student/student_registrationOralDefense');
+const student_assessmentBachelorThesis_router = require('./student/student_assessmentBachelorThesis');
+const student_assessmentOralDefense_router = require('./student/student_assessmentOralDefense');
+
 student_router.use('/get', student_get_router);
 student_router.use('/registrationBachelorThesis', student_registrationBachelorThesis_router);
-student_router.use('/registrationOralDefense', student_registrationOralDefense_router)
+student_router.use('/registrationOralDefense', student_registrationOralDefense_router);
+student_router.use('/assessmentBachelorThesis', student_assessmentBachelorThesis_router);
+student_router.use('/assessmentOralDefense', student_assessmentOralDefense_router);
 module.exports = student_router;
