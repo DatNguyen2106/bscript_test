6048620a7d4509d74339dae0cbcc1ee371943647
DatNguyen2106 <nlddat2000@gmail.com>
2022-12-17 01:12:42 +0700

    17/12

--- server/routers/student/student_get.js
+++ server/routers/student/student_get.js
@@ -55,7 +55,6 @@ student_get_router.post('/lecturers', verifyTokenStudent, async (req, res) =>{
                             "totalPage" : results.chunk(chunkForPage).length,
                             "list" : results.chunk(chunkForPage)[page-1]
                         })}
-                        res.send(results);
                      }
                     else {res.sendStatus(404).send("Unavailable value")}
                 }
@@ -129,6 +128,53 @@ student_get_router.get('/theses', verifyTokenStudent, async (req, res) =>{
                         const queryParamsAssessmentOralDefense = [studentId]
                         const assessmentOralDefenseResults = await executeQuery(res, assessmentOralDefenseQuery,  queryParamsAssessmentOralDefense);  
                         results.pop();
+                        res.send({"list": results[0], registrationBachelorThesisResults, registrationOralDefenseResults, assessmentBachelorThesisResults, assessmentOralDefenseResults});
+                    }               
+                }
+                else res.status(405).send("You are not allowed to access, You are not student")
+            }
+            else res.status(404).send("No user with that username");    
+        } catch (error) {
+            console.log(error.message);
+            res.status(404).send("You got an error" + error.message);
+        }
+        
+    })
+
+student_get_router.get('/thesis', verifyTokenStudent, async (req, res) =>{
+    // because of unique id value, so this api just returns 1 or no value.
+        try {
+            var studentId;
+            var role = req.role;
+            if(req.username && req.userId) {
+                if(role){
+                    if(req.userId === undefined || req.userId === ""){
+                        res.status(404).send("Invalid username with that id")
+                    } else if(!(req.userId) === "number"){
+                        res.status(404).send("Need a number Parameter Id");
+                    } else {
+                        studentId = req.userId;
+                        const query = "CALL getThesisFromStudentId(?)";
+                        const queryParams = [studentId];
+                        const results = await executeQuery(res, query, queryParams);
+                        const registrationBachelorThesisQuery = "SELECT * FROM registrations_for_bachelor_thesis WHERE student_id  = ?";
+                        const queryParamsRegistrationBachelorThesis = [studentId]
+                        const registrationBachelorThesisResults = await executeQuery(res, registrationBachelorThesisQuery,  queryParamsRegistrationBachelorThesis);
+                        
+                        const assessmentBachelorThesisQuery = "SELECT * FROM assessment_for_bachelor_thesis WHERE student_id  = ?";
+                        const queryParamsAssessmentBachelorThesisQuery = [studentId];
+                        const assessmentBachelorThesisResults = await executeQuery(res, assessmentBachelorThesisQuery, queryParamsAssessmentBachelorThesisQuery);
+
+                        const registrationOralDefenseQuery = "SELECT * FROM registrations_for_oral_defense WHERE student_id  = ?";
+                        const queryParamsRegistrationOralDefense = [studentId]
+                        const registrationOralDefenseResults = await executeQuery(res, registrationOralDefenseQuery,  queryParamsRegistrationOralDefense);  
+                        
+                        
+                        const assessmentOralDefenseQuery = "SELECT * FROM assessment_for_oral_defense WHERE student_id  = ?";
+                        const queryParamsAssessmentOralDefense = [studentId]
+                        const assessmentOralDefenseResults = await executeQuery(res, assessmentOralDefenseQuery,  queryParamsAssessmentOralDefense);  
+                        results.pop();
+                        results[0]['student_id'] = studentId;
                         res.send({"list": results[0], registrationBachelorThesisResults, registrationOralDefenseResults, assessmentBachelorThesisResults, assessmentOralDefenseResults});
                     }               
                 }
