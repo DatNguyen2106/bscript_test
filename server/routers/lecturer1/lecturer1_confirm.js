a9afcb2db9be67f9ea08f9f3da5b2e5aee1dd406
DatNguyen2106 <nlddat2000@gmail.com>
2022-12-29 02:11:54 +0700

    update confirmThesis lecturer1

--- server/routers/lecturer1/lecturer1_confirm.js
+++ server/routers/lecturer1/lecturer1_confirm.js
@@ -46,6 +46,40 @@ lecturer1_confirm_router.post('/confirmStudent', verifyTokenLecturer1, async (re
         }
         
     })
+lecturer1_confirm_router.post('/confirmThesis', verifyTokenLecturer1, async (req, res) =>{
+    // because of unique id value, so this api just returns 1 or no value.
+        try {
+            var role = req.role;
+            var thesisId = (req.body.thesisId === undefined || req.body.thesisId === "" || req.body.thesisId === null) ?  null : req.body.thesisId;
+            console.log("before " + req.body.confirmThesis)
+            var confirmThesis = (req.body.confirmThesis === undefined || req.body.confirmThesis === null || req.body.confirmThesis === "") ? false : req.body.confirmThesis;
+            if(req.username && req.userId) {
+                if(role){
+                    const query = "UPDATE lecturers_theses SET confirm_sup2 = ? where thesis_id = ?";
+                    var queryParams;
+                    if(confirmThesis === true) {
+                        queryParams = [true, thesisId];
+                        const results = await executeQuery(res, query, queryParams);
+                        const changeStepQuery = "UPDATE theses SET step = ? where thesis_id = ?";
+                        const changeStepQueryParams = [2, thesisId];
+                        const changeStepResult = await executeQuery(res, changeStepQuery, changeStepQueryParams);
+                        res.send(changeStepResult);
+                    }
+                    else if (confirmThesis === false){
+                        queryParams = [false, thesisId];
+                        const results = await executeQuery(res, query, queryParams);
+                        res.send(results);
+                    }
+                }
+                else res.status(405).send("You are not allowed to access, You are not lecturer1")
+            }
+            else res.status(404).send("No user with that username");    
+        } catch (error) {
+            console.log(error.message);
+            res.status(404).send("You got an error" + error.message);
+        }
+        
+    })
 const executeQuery = (res, query, queryParams) => {
     const results =  new Promise((resolve) => {
         db.query(query, queryParams, (err, result) => {
